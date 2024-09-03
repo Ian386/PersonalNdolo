4fa7d2dfe6e81b01856a6d2f56ff1f41bc93381f
Ian Ndolo Mwau <134711923+Ian386@users.noreply.github.com>
2024-09-03 19:27:06 +0300

    leetcode 2

---
+++ LeetCode Prac/ltcode2.py
@@ -0,0 +1,19 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    shortest = min(strs, key=len)
+    for i, ch in enumerate(shortest):
+        for other in strs:
+            if other[i] != ch:
+                return shortest[:i]
+    return shortest
+
+
+def longestCommonPrefix2(strs):
+    res = ""
+    for i in range(len(strs[0])):
+        for s in strs:
+            if i == len(s) or s[i] != strs[0][i]:
+                return res
+        res += strs[0][i]
+    return res
